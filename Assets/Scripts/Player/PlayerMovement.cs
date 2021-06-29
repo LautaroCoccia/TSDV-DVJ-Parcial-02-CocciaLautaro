72b29835045a08274c2de7d3eb5bde8a2cea2a46
LautaroCoccia <lautaroauditore@gmail.com>
2021-06-28 23:58:23 -0300

    Add player movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float rotationForce;
+    [SerializeField] float powerForce;
+    float horizontalTurn;
+    float upVariable;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalTurn = Input.GetAxis("Horizontal");
+        transform.Rotate(new Vector3(0, 0, rotationForce * horizontalTurn * Time.deltaTime));
+        upVariable = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        if(upVariable >0)
+            rb.AddForce(transform.up * powerForce);
+    }
+}
